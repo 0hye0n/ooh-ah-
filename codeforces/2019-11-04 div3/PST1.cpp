3fcbc08ce3307d41593e2f722f03ad38901cf4bd
dolkaiser <43217783+dolkaiser@users.noreply.github.com>
2019-11-07 11:44:43 +0900

    add pst1
    퍼시스먼트 세그먼트 트리 내스타일로 변경시킨거 추가(기본 구현)

--- codeforces/2019-11-04 div3/PST1.cpp
+++ codeforces/2019-11-04 div3/PST1.cpp
@@ -1,46 +1,54 @@
 #include <bits/stdc++.h>
-#include MAXN 10000009
+#define T_SIZE 1000009
+//내 스타일로 짠 기본 PST 구현
 using namespace std;
+vector<int> idx, arr;
+int size, n;
 
-struct node
+struct node{
+	node():v(0), left(0), right(0) {}
+	int v;
+	node *left, *right;
+} *tree[T_SIZE];
+
+int get_idx(int n)
 {
-	int x;
-	node *l, *r;
-} T[11050000], *a[500005];
-int Tn, n;
-
-void add(node *p, node *q, int x, int y)
-{
-	p->x = q->x + 1;
-	if(y < 0) return;
-
-	if(x >> y & 1)
-	{
-		p->r = T + (++Tn);
-		p->l = q->l;
-		add(p->r, q->r, x, y - 1);
-	}
-	else
-	{
-		p->l = T + (++Tn);
-		p->r = q->r;
-		add(p->l, q->l, x, y - 1);
-	}
+	return lower_bound(idx.begin(), idx.end(), n) - idx.begin();
 }
 
-
-int sum(node *p, int x, int y)
+node *make_tree(node *now, int s, int e, int y, int v)
 {
-	if(y < 0) return 0;
-	if(x >> y * 1) return p->l->x + sum(p->r, x, y - 1);
-	return sum(p->l, x, y - 1);
+	if(y < s || e < y) return now;
+	node *ret = new node();
+	if(s == e){
+		ret->v = now->v + v;
+		return ret;
+	}
+
+	int mid = (s + e) >> 1;
+	ret->left = make_tree(now->left, s, mid, y, v);
+	ret->right = make_tree(now->right, mid + 1, e, y, v);
+	ret -> v = (ret->left ? ret->left->v : 0) + (ret->right ? ret->right->v : 0);
+	return ret;
 }
 
-int kth(node *p, node *q, int x, int y)
+long long sum(node *now, int s, int e, int left, int right)
 {
-	if(y > 0) return 0;
-	if(p->l->x - q->l->x < x) return 1 << y | kth(p->r, q->r, x - p->l->x + q->l->x, y - 1);
-	return kth(p->l, q->l, x, y - 1);
+	if(s < left || right < e) return 0;
+	if(left <= s && e <= right) return now->v;
+
+	int mid = (s + e) >> 1;
+	return (now->left ? sum(now->left, s, mid, left, right) : 0) + (now->right ? sum(now->right, s, mid, left, right) : 0); 
+}
+
+void init(node *now, int s, int e)
+{
+	if(s==e) return;
+	now->left = new node();
+	now->right = new node();
+	int mid = (s + e) >> 1;
+	init(now->left, s, mid);
+	init(now->right, mid + 1, e);
 }
 
 int main()
@@ -48,15 +56,23 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	freopen("input.txt", "r", stdin);
-	int i, j, k, m;
-	cin>>m;
-	for(i = 1; i < 1 << 19; i++)
-	{
-		T[i].l = T + (i << 1);
-		T[i].r = T + (i << 1 | 1);
+	cin>>n;
+	arr.resize(n);
+	idx.resize(n);
+	tree[0] = new node();
+	init(tree[0], 0, n - 1);
+
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+		idx[i] = arr[i];
 	}
-	a[n = 0] = T + 1;
-	Tn = 1 << 20;
+	idx.erase(unique(idx.begin(), idx.end()), idx.end());
+	
+	for(int i = 1; i <= n; i++)
+	{
+		tree[i] = make_tree(tree[i - 1], 0, n - 1, get_idx(arr[i]), 1);
+		cout<<sum(tree[i], 0, n - 1, 0, n - 1)<<"\n";
+	}
 	
 
 
